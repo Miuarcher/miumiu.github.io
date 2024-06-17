eec0ab5dd88de5d578cafc387c097dc6bd09acc0
x2rs <141597583+DreamSnow607@users.noreply.github.com>
2024-06-17 23:23:09 +0800

    add boldsymbol macro

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -1,19 +1,26 @@
 window.MathJax = {
-    tex: {
-      inlineMath: [["\\(", "\\)"]],
-      displayMath: [["\\[", "\\]"]],
-      processEscapes: true,
-      processEnvironments: true
-    },
-    options: {
-      ignoreHtmlClass: ".*|",
-      processHtmlClass: "arithmatex"
-    }
-  };
+  // [加载boldsymbol宏包]
+  // https://docs.mathjax.org/en/latest/input/tex/extensions/boldsymbol.html
+  loader: {load: ['[tex]/boldsymbol']},
+
+  tex: {
+    // 加载boldsymbol宏包
+    packages: {'[+]': ['boldsymbol']},
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
   
-  document$.subscribe(() => { 
-    MathJax.startup.output.clearCache()
-    MathJax.typesetClear()
-    MathJax.texReset()
-    MathJax.typesetPromise()
-  })
+document$.subscribe(() => { 
+  MathJax.startup.output.clearCache()
+  MathJax.typesetClear()
+  MathJax.texReset()
+  MathJax.typesetPromise()
+})
